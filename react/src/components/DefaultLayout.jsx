5ab2706ff05097b8aaa2123117e8aac7be0bba02
MuhamedAly7 <muhammedaly021@gmail.com>
2024-11-22 23:39:46 +0200

    prepare frontend components

---
+++ react/src/components/DefaultLayout.jsx
@@ -0,0 +1,37 @@
+import { Link, Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../context/ContextProvider";
+
+export default function DefaultLayout() {
+    const { user, token } = useStateContext();
+
+    if (!token) {
+        return <Navigate to="/login" />;
+    }
+
+    const onLogout = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div id="defaultLayout">
+            <aside>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/users">User</Link>
+            </aside>
+            <div className="content">
+                <header>
+                    <div>Header</div>
+                    <div>
+                        {user.name}
+                        <a href="#" className="btn-logout" onClick={onLogout}>
+                            Logout
+                        </a>
+                    </div>
+                </header>
+                <main>
+                    <Outlet />
+                </main>
+            </div>
+        </div>
+    );
+}
---
+++ react/src/views/Users.jsx
@@ -0,0 +1,9 @@
+function Users() {
+    return (
+        <>
+            <h1>Hello Users</h1>
+        </>
+    );
+}
+
+export default Users;
