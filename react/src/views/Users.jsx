fa9947c928a54663dcfffafea87fed73ad745d8e
MuhamedAly7 <muhammedaly021@gmail.com>
2024-11-23 06:15:39 +0200

    Signup and Login

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
+import axiosClient from "../axios-client";
+
 function Users() {
+    const [users, setUsers] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+    const getUsers = () => {
+        setLoading(false);
+        axiosClient.get('/users')
+        .then(({data}) => {
+            // setUsers(data);
+            console.log(data);
+        })
+        .catch(() => {
+            setLoading(false)
+        })
+    }
+
     return (
-        <>
-            <h1>Hello Users</h1>
-        </>
+        <div>
+            Users
+        </div>
     );
 }
 
